28ec182334f4c793d376ce4f2526cf109f824b9c
René Schwaiger <sanssecours@f-m.fm>
2016-12-15 14:37:18 +0100

    Formula: Add `ronn` as build dependency

--- Formula/elektra.rb
+++ Formula/elektra.rb
@@ -5,14 +5,19 @@ class Elektra < Formula
   sha256 "cc14f09539aa95623e884f28e8be7bd67c37550d25e08288108a54fd294fd2a8"
   head "https://github.com/ElektraInitiative/libelektra.git"
 
-  depends_on "cmake" => :build
-
   bottle do
     root_url("https://github.com/ElektraInitiative/homebrew-elektra/releases/" \
              "download/0.8.19")
     sierra = "f55334d35230b1c693cb42f049f19ed27a78d52d8ffdbee0ec01c934b8f2c7a7"
     sha256(sierra => :sierra)
   end
+
+  env :userpaths
+
+  depends_on "cmake" => :build
+  ronn = LanguageModuleRequirement.new :ruby, "ronn"
+  ronn.tags << :build
+  depends_on ronn
 
   def install
     cmake_args = %W[
